764bf66dcd9ff3fc988e81f0b5bba2fb80aa8d92
JO WONYOUNG <whdnjsdud551@gmail.com>
2025-05-18 03:22:18 +0900

    feat: WeaherTime 컴포넌트 정의

---
+++ src/components/WeatherTime/WeatherTime.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WeatherBox from '../common/WeatherBox';
+
+const WeatherTime = () => {
+  return (
+    <WeatherBox title="시간별 현황">
+      <div>날씨앙</div>
+    </WeatherBox>
+  );
+};
+
+export default WeatherTime;
