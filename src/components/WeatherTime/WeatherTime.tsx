7404defda7eb265221f87182c48d4b560923e0c4
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-05-18 21:39:28 +0900

    feat: 차트 라인 렌더

--- src/components/WeatherTime/WeatherTime.tsx
+++ src/components/WeatherTime/WeatherTime.tsx
@@ -3,25 +3,47 @@ import WeatherBox from '../common/WeatherBox';
 import useWeatherLocation from '../../hooks/useWeatherLocation';
 import { useQuery } from '@tanstack/react-query';
 import { getWeaherHourly } from '../../apis/Weather/WeatherHourly';
+import { Line, LineChart, ResponsiveContainer } from 'recharts';
+import styled from 'styled-components';
 
 const WeatherTime = () => {
   const { location } = useWeatherLocation();
 
-  const { data, isPending } = useQuery({
+  const { data } = useQuery({
     queryKey: ['weather', 'hourly', location],
     queryFn: () => getWeaherHourly(location),
     enabled: !!location,
   });
 
-  if (!isPending) {
-    console.log('data', data);
-  }
+  const weatherHourlyData = data?.hourly.slice(12);
 
   return (
     <WeatherBox title="시간별 현황">
-      <div>ㅎ앟얗ㅇ</div>
+      <WeatherTimeWrapper>
+        <ResponsiveContainer width="100%" height={100}>
+          <LineChart
+            data={weatherHourlyData?.map(hourWeahter => {
+              return {
+                name: hourWeahter.time,
+                pv: Math.floor(hourWeahter.temp),
+              };
+            })}
+          >
+            <Line
+              type="monotone"
+              dataKey="pv"
+              stroke="#D6D6D6"
+              strokeWidth={2}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </WeatherTimeWrapper>
     </WeatherBox>
   );
 };
+
+const WeatherTimeWrapper = styled.div`
+  padding: 12px 24px;
+`;
 
 export default WeatherTime;
