ba17a304ecd9cd6f5cd5f5c2ea50dd4c751f0e74
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-05-19 01:46:17 +0900

    chore: localhost https 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
+export default defineConfig(({ mode }) => {
+  // 환경 변수 불러오기 (필요시)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      react(),
+      svgr(),
+      env.VITE_MODE !== 'development' && mkcert(), // 조건부 플러그인 추가
+    ].filter(Boolean), // false/null 제거
+  };
 });
